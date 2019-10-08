c0e52f28574a5b604ffc1a0407c9c6f19f409ac2
JnAlbHer <43509640+JnAlbHer@users.noreply.github.com>
2019-10-08 12:50:12 -0500

    PRIMERO

---
+++ src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MiComponente msg="Componente PARA Reuso" :posts="posts"/>
+    <h1>Componente CON Reuso</h1>
+    <div class="divisor-graficas">
+      <h2 style="color:white">Ventas</h2>
+      <VueFunnelGraph :width="width" :height="height" :labels="labels"
+                    :values="values" :colors="colors" :sub-labels="subLabels" :direction="direction"
+                    :gradient-direction="gradientDirection"
+                    :animated="true" :display-percentage="true"
+      ></VueFunnelGraph>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from './components/MiComponente.vue'
+import {VueFunnelGraph} from 'vue-funnel-graph-js'
+// <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    MiComponente,
+    VueFunnelGraph
+  },
+
+  data () {
+    return {
+      posts: {
+        1:{
+          title: "Producto 1",
+          content: "Ingresa una descripción del producto 1",
+          link: "/",
+          linkLabel:"Comprar $1.00",
+          image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbmCfLriVQzn0B8Cgrc8fjsBdmYsl3R68FMrSdH8VG6vCwr7kS',
+        },
+        2:{
+          title: "Producto 2",
+          content: "Ingresa una descripción del producto 2",
+          link: "/",
+          linkLabel:"Comprar $2.00",
+          image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbmCfLriVQzn0B8Cgrc8fjsBdmYsl3R68FMrSdH8VG6vCwr7kS',
+        },
+        3:{
+          title: "Producto 3",
+          content: "Ingresa una descripción del producto 3",
+          link: "/",
+          linkLabel:"Comprar $3.00",
+          image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbmCfLriVQzn0B8Cgrc8fjsBdmYsl3R68FMrSdH8VG6vCwr7kS',
+        },
+        4:{
+          title: "Producto 4",
+          content: "Ingresa una descripción del producto 4",
+          link: "/",
+          linkLabel:"Comprar $4.00",
+          image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbmCfLriVQzn0B8Cgrc8fjsBdmYsl3R68FMrSdH8VG6vCwr7kS',
+        },
+        5:{
+          title: "Producto 5",
+          content: "Ingresa una descripción del producto 5",
+          link: "/",
+          linkLabel:"Comprar $5.00",
+          image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbmCfLriVQzn0B8Cgrc8fjsBdmYsl3R68FMrSdH8VG6vCwr7kS',
+        },
+        6:{
+          title: "Producto 6",
+          content: "Ingresa una descripción del producto 6",
+          link: "/",
+          linkLabel:"Comprar $6.00",
+          image:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbmCfLriVQzn0B8Cgrc8fjsBdmYsl3R68FMrSdH8VG6vCwr7kS',
+        },
+      },
+      labels: ['Impressions', 'Add To Cart', 'Buy'],
+      subLabels: ['Direct', 'Social Media', 'Ads'],
+          values: [
+          // with the given Labels and SubLabels here's what the values represent:
+          // 
+          // Direct, Social, Ads  
+          //    |      |     |  
+          //    v      v     v
+              [3000, 2500, 6500], // Segments of "Impressions" from top to bottom
+              [3000, 1700, 1000], // Segments of "Add To Cart"
+              [600,  200,  130]   // Segments of "Buy"
+          ],
+          colors: [
+              ['#FFB178', '#FF3C8E'], // color set for "Impressions" segment
+              ['#A0BBFF', '#EC77FF'], // color set for "Add To Cart" segment
+              ['#A0F9FF', '#7795FF']  // color set for "Buy" segment
+          ],
+          direction: 'horizontal',
+          gradientDirection: 'horizontal',
+          height: 300,
+          width: 800,
+    }
+  }
+}
+</script>
+
+<style>
+.divisor-graficas{
+  margin-bottom: 60px;
+  padding: 32px;
+  background-color: #393862;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /*color: #2c3e50;*/
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="MiComponente">
+    <h1>{{ msg }}</h1>
+    <div class="cards-container">
+      <div class="card" style="width: 18rem;" v-for="post in posts" v-bind:key="post.key">
+        <img :src="post.image" class="card-img-top" alt="...">
+        <div class="card-body">
+          <div class="card-body-center">
+            <h5 class="card-title"><b>{{post.title}}</b></h5>
+            <p class="card-text">{{post.content}}</p>
+          </div>
+          <a :href="post.content" class="btn btn-primary">{{post.linkLabel}}</a>
+        </div>
+      </div>
+    </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MiComponente',
+  props: {
+    msg: String,
+    
+    posts: {
+      type: Object,
+      default: function () {
+        return[
+          // title: 'Title',
+          // content: 'Place here the content',
+          // image:'./assets/logo.png'
+          // <h1 v-for="post in posts" v-bind:key="post.key">{{post}}</h1>
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+/*a {
+  color: #42b983;
+}*/
+.card{
+  margin: 32px !important;
+}
+.card:hover{
+  transform: scale(1.2);
+  border: 2px solid;
+}
+.cards-container{
+  display: flex;
+}
+.card-body-center{
+  background-color: rgb(250, 247, 247);
+  padding: 4px;
+  margin: 8px 4px;
+}
+</style>
